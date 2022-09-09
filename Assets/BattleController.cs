c6adea2af85b51076dad6782efbe2e8072800978
yukito94 <107991395+yukito94@users.noreply.github.com>
2022-09-09 20:19:38 +0900

    Initial commit

---
+++ Assets/BattleController.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BattleController : MonoBehaviour
+{
+    //スライダーで攻撃の見える化を図る
+    public Slider HPGauge;
+
+    //ユニットが何を持っているかを宣言する
+    int hp; //現在のhp
+    int maxHp = 1000;  //最大のhp(本作は敵味方共に1000とする)
+
+    public int attack; //攻撃力
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    hp = maxHp; //初期値はそのまま
+
+    HPGauge.maxValue = maxHp;
+    HPGauge.value = maxHp;
+    
+    }
+
+
+    //ダメージの見える化
+    public void onDamage(int _damage){
+        hp -= _damage;
+
+        //スライダーの設定
+        //HPが0以下になった時
+        if(hp <= 0){
+            hp = 0;
+        }
+        HPGauge.value = hp;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    //攻撃力はその都度変更する必要がある為、ここに記載（毎秒１回攻撃する事となっているがUpdateはフレームで動いてるため、どう判定されてるか要調査）
+    attack = Random.Range(1, 99);    //攻撃力
+
+    //（前衛と後衛の概念を追加する場合はここに記載する）
+        
+    }
+}
---
+++ Assets/BattleSystem.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//オートバトルが進行できるようここに記述を進めてく
+
+public class BattleSystem : MonoBehaviour
+{
+    //おおよその時間設定（一定時間で攻撃しあう）
+    private float xtime;
+
+    //取得する情報
+    //publicを追加することで他のデーター(この場合BattleController)から呼び込める
+    public BattleController player;
+    public BattleController enemy;
+
+    //攻撃反撃
+    bool PlayerTurn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //開始時はplayerが先行に設定
+        PlayerTurn = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    //再生端末の性能・環境に偏らず一定時間で攻撃できる（Time.deltaTime）
+    xtime -= Time.deltaTime;
+    if (xtime <= 0.0) {
+        xtime = 1.0f;
+
+        //ここからバトル周りの処理
+        //playerがenemyを攻撃する
+        enemy.onDamage(player.attack);
+        PlayerTurn = false;
+
+        Debug.Log("Enemyダメージ" + player.attack);
+    }
+
+    //エネミーの攻撃に秒数は関係ない為、秒数のif文の外に記載する
+    if(!PlayerTurn){
+        PlayerTurn = true;
+        player.onDamage(enemy.attack);
+        
+        Debug.Log("Playerダメージ" + enemy.attack);
+    }
+        
+    }
+}
